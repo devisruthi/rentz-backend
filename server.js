f65f65f1954e8ebbc1a5f7cd8d0efda5256f8b33
Sruthi <devisruthi@gmail.com>
2020-12-18 17:43:25 +0400

    Add a mongodb connection

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 // Import the main express file as a function
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Invoke express
 const server = express();
 
+const dbString = "mongodb+srv://admin01:db12345@cluster0.kzgjt.mongodb.net/rentz?retryWrites=true&w=majority";
+
+mongoose
+    .connect(dbString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(
+        () => {
+            console.log('db is connected')
+        }
+    )
+    .catch(
+        (error) => {
+            console.log('db is NOT connected. An error occured.', error)
+        }
+    )
+
+
 server.get(
     '/',
     (req, res) => {
-        res.send("<h1>Welcome to Rentz</h1>")
+        res.send("index.html")
     }
 );
 
+server.get(
+    '*',
+    (req, res) => {
+        res.send('<h1>404</h1>')
+    }
+);
+
+
+// Connects a port number on the server
 server.listen(
     3001, 
     ()=>{
         console.log('server is running on http://localhost:3001');
     }
-)
+);
